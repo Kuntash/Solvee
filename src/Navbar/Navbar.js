2d04c4f9093b8251088fc5f9b55a0856f37053e0
Kuntash <kuntashtashi11@gmail.com>
2021-11-10 04:00:28 +0530

    Navbar design mockup

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,8 +1,15 @@
+import Navitem from './Navitem';
 const Navbar = () => {
+    const navItem = ['Explore', 'Questions', 'Rating', 'Notification', 'Settings'];
     return (
-        <nav>
-            
+        <nav className = "nav">
+            <div className = "nav-brand"> Logo</div>
+            {
+                navItem.map((item, i) => <Navitem key = {i} title = {item} />)
+            }
+            <p>version 1.0.0</p>
         </nav>)
 }
 
 export default Navbar;
+
---
+++ src/Navbar/Navitem.js
@@ -0,0 +1,9 @@
+const Navitem = (props) => {
+    return (
+        <div className = "nav-item">
+            {props.title}
+        </div>
+    );
+}
+
+export default Navitem;
